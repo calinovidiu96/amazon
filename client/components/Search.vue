99bc3c021914fa75eee3da496960dce641103c96
calinovidiu96 <calinovidiu96@gmail.com>
2020-09-14 10:14:57 +0300

    searchbar

--- client/components/Search.vue
+++ client/components/Search.vue
@@ -12,8 +12,8 @@
 
 <form novalidate="novalidate" onsubmit="return false;" class="searchbox sbx-amazon">
   <div role="search" class="sbx-amazon__wrapper">
-    <input type="search" name="search" placeholder="Search your website" autocomplete="off" required="required" class="sbx-amazon__input">
-    <button type="submit" title="Submit your search query." class="sbx-amazon__submit">
+    <input type="search" name="search" placeholder="Search your website" autocomplete="off" required="required" class="sbx-amazon__input" v-model="query">
+    <button type="submit" title="Submit your search query." class="sbx-amazon__submit" @click="onSearch ">
       <svg role="img" aria-label="Search">
         <use xlink:href="#sbx-icon-search-11"></use>
       </svg>
@@ -27,3 +27,20 @@
 </form>   
     </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      query: ''
+    }
+  },
+  methods: {
+    onSearch(){
+      this.$router.push({
+        path: "/search",
+        query: { title: this.query }
+      });
+    }
+  }
+}
+</script>
--- server/server.js
+++ server/server.js
@@ -34,6 +34,7 @@ const reviewRoutes = require('./routes/review');
 const addressRoutes = require('./routes/address'); 
 const paymentRoutes = require('./routes/payment'); 
 const orderRoutes = require('./routes/order'); 
+const searchRoutes = require('./routes/search'); 
 
 
 
@@ -45,6 +46,7 @@ app.use('/api', reviewRoutes);
 app.use('/api', addressRoutes);
 app.use('/api', paymentRoutes);
 app.use('/api', orderRoutes);
+app.use('/api', searchRoutes);
 
 
 
